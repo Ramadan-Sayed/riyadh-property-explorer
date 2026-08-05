437f35bf4d44c85993cfbe56f41e0bdf4ae281f2
Ramadan <ramadansayed161@gmail.com>
2026-08-06 00:51:56 +0300

    feat(survey): add mock survey station data

---
+++ src/data/surveyStationsData.ts
@@ -0,0 +1,7 @@
+import { SurveyStation } from '../types/survey';
+
+export const mockSurveyStations: SurveyStation[] = [
+  { id: 'st1', code: 'ST-001', coordinates: [24.7136, 46.6753], elevation: 620 },
+  { id: 'st2', code: 'ST-002', coordinates: [24.7180, 46.6800], elevation: 615 },
+  { id: 'st3', code: 'ST-003', coordinates: [24.7100, 46.6700], elevation: 622 }
+];
